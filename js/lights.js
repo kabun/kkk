17fd1abbe4cadc628b9ee7d8676f824b650b1b5b
kabun <cococodm@gmail.com>
2018-03-03 21:32:39 +0900

    プルリク対応

--- js/lights.js
+++ js/lights.js
@@ -1,43 +1,37 @@
-$(window).on('load', function(){
+$(function(){
+
   // 初期化
-  var timer = false;
-  var windowHeight;
-  var windowHeightHalf;
-  var windowWidth;
-  var windowWidthHalf;
+  var _global = $(window);
+  var GLOBALMENUHEIGHT  = 61;  // グローバルメニューの縦幅
+  var LIGHTCOUNT        = 12;  // 光の数
+  var DELAYWINDOWRESIZE = 200; // ウィンドウリサイズに、光を表示する時差
 
-  // ウィンドウ幅を取得
-  function getWindowHeight(){
-    windowHeight = $(window).height();
-    windowHeightHalf = $(window).height() / 2;
-    windowWidth = $(window).height();
-    windowWidthHalf = $(window).height() / 2;
-  }
+  // セレクタ
+  var _headerLights     = ".header-lights";
+  var _headerLightsArea = ".header-lights-area";
+  var _particle         = ".particle";
 
-  // ウィンドウ幅の初期値を取得
-  getWindowHeight();
-  headerLights();
+  // ページ読み込み時、光が振ってくるアニメーションを開始
+  window.addEventListener("load", headerLights, false);
 
-  // ウィンドウサイズを変更時、時差をつけてウィンドウ幅を取得
-  $(window).on('resize', function(){
-    $(".particle").remove();
-    if (timer !== false) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(function() {
-      getWindowHeight();
-      headerLights();
-    }, 200);
-  });
+  // 一定量スクロールすると、光を削除したり、再描画する
+  window.addEventListener("scroll", lightsHiedden, false);
+
+  // ウィンドウリサイズ時、光を再描画
+  window.addEventListener("resize", resizeWindow, false);
 
   // 光の表示位置、種類、タイミングをランダムで生成
   function headerLights() {
-    $.each($(".header-lights"), function(){
-      //光の数
-      var LIGHTCOUNT = 12;
+    // ウィンドウ幅を取得
+    var windowHeight     = _global.height();
+    var windowHeightHalf = _global.height() / 2;
+    var windowWidth      = _global.height();
+    var windowWidthHalf  = _global.height() / 2;
+
+    $.each($(_headerLights), function(){
       for(var i = 0; i <= LIGHTCOUNT; i++) {
         //光が出現するタイミング
-        var animationDelay = Math.random()*10;
+        var animationDelay   = Math.random()*10;
         //X座標、Y座標
         var defaultPositionX = (function(){
           return Math.floor(Math.ceil(Math.random()*windowWidth)-windowWidthHalf);
@@ -46,29 +40,44 @@ $(window).on('load', function(){
           return Math.floor(Math.ceil(Math.random()*windowHeight)-windowHeightHalf);
         })();
         //スピード
-        var lightType = Math.floor(Math.random()*2);
+        var lightType  = Math.floor(Math.random()*2);
         var lightSpeed = Math.floor(Math.random()*2);
-        if (lightSpeed) {
-          rorateWay = "rorate-way-fast";
-        } else {
-          rorateWay = "rorate-way-low";
-        }
+
+        var rorateWay  = (lightSpeed) ? "rorate-way-fast" : "rorate-way-low";
+
         //光のオブジェクトを生成
-        $(this).append('<span class="particle ' + rorateWay +'" style="top:' + defaultPositionY + 'px; left:' + defaultPositionX + 'px;-webkit-animation-delay: ' + animationDelay + 's;animation-delay: ' + animationDelay + 's;"><img src="./img/light_' + lightType + '.png" width="15" height="15" alt=""></span>');
+        $(this).append('<span class="particle ' + rorateWay +
+        '" style="top:' + defaultPositionY +
+        'px; left:' + defaultPositionX +
+        'px;-webkit-animation-delay: ' + animationDelay +
+        's;animation-delay: ' + animationDelay +
+        's;"><img src="./img/light_' + lightType +
+        '.png" width="15" height="15" alt=""></span>');
       }
     });
   }
-  //一定量スクロールすると、光を非表示
-  $(window).scroll(function(){
-    // 光を非表示にするスクロール量
-    var distanceTop = windowHeight;
-    // スクロール位置に達しているかどうかを判別
-    if ($(window).scrollTop() > distanceTop - 61) {
-      // 光を非表示
-      $('.header-lights-area').css('display', 'none');
-    } else {
-      // 光を表示
-      $('.header-lights-area').css('display', 'block');
+
+  // ウィンドウサイズを変更時、時差をつけてウィンドウ幅を取得
+  function resizeWindow(){
+    // 時差用
+    var timer = false;
+    $(_particle).remove();
+    if (timer !== false) {
+      clearTimeout(timer);
     }
-  });
+    timer = setTimeout(function() {
+      headerLights();
+    }, DELAYWINDOWRESIZE);
+  }
+
+  // 光を表示・非表示切り替え
+  function lightsHiedden(){
+    // 光を非表示にするスクロール量
+    var distanceTop = windowHeight = _global.height();
+    // スクロール位置に達しているかどうかを判別して、表示切り替え
+    var css = (_global.scrollTop() > distanceTop - GLOBALMENUHEIGHT) ? 'block' : 'none';
+    // 光を非表示
+    $(_headerLightsArea).css('display', css);
+  }
+
 });
