7efa20cd4c1c919e5e5eb259411979d1d8526311
kabun <cococodm@gmail.com>
2018-03-03 22:53:53 +0900

    変数名、関数名 修正

--- js/lights.js
+++ js/lights.js
@@ -7,28 +7,28 @@ $(function(){
   var DELAY_WINDOW_RESIZE = 200; // ウィンドウリサイズに、光を表示する時差
 
   // セレクタ
-  var _headerLights     = ".header-lights";
-  var _headerLightsArea = ".header-lights-area";
-  var _particle         = ".particle";
+  var headerLights     = ".header-lights";
+  var headerLightsArea = ".header-lights-area";
+  var particle         = ".particle";
 
   // ページ読み込み時、光が振ってくるアニメーションを開始
-  window.addEventListener("load", headerLights, false);
+  window.addEventListener("load", addHeaderLights, false);
 
   // 一定量スクロールすると、光を削除したり、再描画する
-  window.addEventListener("scroll", lightsHiedden, false);
+  window.addEventListener("scroll", hiddenLights, false);
 
   // ウィンドウリサイズ時、光を再描画
   window.addEventListener("resize", resizeWindow, false);
 
   // 光の表示位置、種類、タイミングをランダムで生成
-  function headerLights() {
+  function addHeaderLights() {
     // ウィンドウ幅を取得
     var windowHeight     = _global.height();
     var windowHeightHalf = _global.height() / 2;
     var windowWidth      = _global.height();
     var windowWidthHalf  = _global.height() / 2;
 
-    $.each($(_headerLights), function(){
+    $.each($(headerLights), function(){
       for(var i = 0; i <= LIGHT_COUNT; i++) {
         //光が出現するタイミング
         var animationDelay   = Math.random()*10;
@@ -61,23 +61,23 @@ $(function(){
   function resizeWindow(){
     // 時差用
     var timer = false;
-    $(_particle).remove();
+    $(particle).remove();
     if (timer !== false) {
       clearTimeout(timer);
     }
     timer = setTimeout(function() {
-      headerLights();
+      addHeaderLights();
     }, DELAY_WINDOW_RESIZE);
   }
 
   // 光を表示・非表示切り替え
-  function lightsHiedden(){
+  function hiddenLights(){
     // 光を非表示にするスクロール量
     var distanceTop = windowHeight = _global.height();
     // スクロール位置に達しているかどうかを判別して、表示切り替え
     var css = (_global.scrollTop() > distanceTop - GLOBAL_MENU_HEIGHT) ? 'none' : 'block';
     // 光を非表示
-    $(_headerLightsArea).css('display', css);
+    $(headerLightsArea).css('display', css);
   }
 
 });
