e0839c5bdc820017556b9f4336c224148adf4214
kabun <cococodm@gmail.com>
2018-03-22 00:45:02 +0900

    アローを自動生成

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,11 @@
-$(function(){
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(function() {
   // 初期化
   var currentScrollTop,   // スクロール位置 記録用
+      activeModalIdNum,   // アクティブモーダルID 記録用
+      activeModalType,    // アクティブモーダルの種類 記録用
       timer        = false,
       DELAY_RESIZE = 200, // ウィンドウリサイズ時に、光を表示する時差
       FADEIN_TIME  = 200, // フェードイン時間
@@ -11,39 +16,31 @@ $(function(){
   var modalOverlay = ".modal-overlay",
       modalInner   = ".modal-inner",
       modalClose   = ".modal-close",
-      globalNav    = ".global-nav-wrapper",
-      modalArrow   = ".modal-arrow";
+      globalNav    = ".global-nav-wrapper";
 
   // DOM
-  var $_global      = $(window),
-      $_body        = $('body'),
+  var $window       = $(window),
+      $document     = $(document),
+      $body         = $('body'),
       $modalOverlay = $(modalOverlay),
       $modalInner   = $(modalInner),
       $modalClose   = $(modalClose),
-      $globalNav    = $(globalNav),
-      $modalArrow   = $(modalArrow);
+      $globalNav    = $(globalNav);
 
   // ウィンドウリサイズ時、モーダルもリサイズ
-  $_global.on('resize', function(){
+  $window.on('resize', function() {
     if (timer !== false) {
       clearTimeout(timer);
     }
     timer = setTimeout(function() {
-      var _height = $_global.height();
+      var _height = $window.height();
       $modalOverlay.css('height', _height);
       $modalInner.css(  'height', _height - ADJUST_HIGHT + "px");
     }, DELAY_RESIZE);
   });
 
-  // arrowにカーソルが重なったら、arrowの色を変更
-  $(modalArrow).hover(function(){
-    $(this).addClass('hover');
-  },function(){
-    $(this).removeClass('hover');
-  });
-
   // サムネイル画像押下時、モーダルを表示。または アローを押下時、モーダル内容を変更
-  $(document).on('click', '[data-openmodal]', function(e) {
+  $document.on('click', '[data-openmodal]', function(e) {
     if ($(this).data('openmodal-type') === "arrow") {
       // モーダルをarrowで開いた場合
       // 動画再生を止める
@@ -58,46 +55,81 @@ $(function(){
       // グローバルナビを隠す
       $globalNav.hide();
       //スクロール位置を記録
-      currentScrollTop = $_global.scrollTop();
+      currentScrollTop = $window.scrollTop();
       // bodyのスクロール禁止
-      $($_body).css({
+      $body.css({
         'position': 'fixed',
         'width'   : '100%',
         'overflow': 'hidden'
       });
     }
     // モーダル表示
-    var _height = $_global.height();
-    var targetOpenModalId  = $(this).data('openmodal');
-    var $targetOpenOverlay = $('#' + targetOpenModalId);
-    var $targetOpenInner   = $('#' + targetOpenModalId + ' ' + modalInner);
+    var _height = $window.height();
+        targetOpenModalName  = $(this).data('openmodal'),
+        $targetOpenOverlay   = $('#' + targetOpenModalName),
+        $targetOpenInner     = $('#' + targetOpenModalName + ' ' + modalInner);
     $targetOpenOverlay.addClass('active-modal');
     $targetOpenOverlay.css({'height': _height + 'px'});
     $targetOpenInner.css({  'height': (_height - ADJUST_HIGHT) + 'px'});
     $targetOpenOverlay.fadeIn(FADEIN_TIME);
+
+    // arrowの生成
+    var activeModalIdNum   = targetOpenModalName.slice(-1),
+        activeModalName    = targetOpenModalName.slice(0,-1),
+        activeModalTypeNum = $('.' + activeModalType).length;
+        prevModalIdNum     = (Number(activeModalIdNum)-1),
+        nextModalIdNum     = (Number(activeModalIdNum)+1),
+        prevModalName      = activeModalName + prevModalIdNum,
+        nextModalName      = activeModalName + nextModalIdNum,
+        min                = 1,
+        max                = (Number($('.' + activeModalName).length));
+    if(activeModalIdNum <= max && activeModalIdNum > min) {
+      $targetOpenOverlay.find('.section-title').prepend(
+        '<div class="modal-arrow arrow-box arrow-left" ' +
+        'data-openmodal="' + prevModalName + '" data-openmodal-type="arrow"></div>'
+      );
+    }
+    if(activeModalIdNum >= min && activeModalIdNum < max) {
+      $targetOpenOverlay.find('.section-title').prepend(
+        '<div class="modal-arrow arrow-box arrow-right" ' +
+        'data-openmodal="' + nextModalName + '" data-openmodal-type="arrow"></div>'
+      );
+    }
+
     // モーダルウィンドウのスクロール位置をリセット
     $targetOpenInner.scrollTop(0);
   });
 
   //「xボタン」または、「閉じるボタン」押下時、モーダルを隠す
-  $(document).on('click', '[data-closemodal]', function(e) {
+  $document.on('click', '[data-closemodal]', function(e) {
     // グローバルナビを表示
-    $globalNav.fadeIn(FADEIN_TIME);
+    $globalNav.fadeIn
+    (FADEIN_TIME);
     // bodyのスクロール禁止解除
-    $($_body).css({
+    $body.css({
       'position': 'relative',
       'width'   : '',
       'overflow': ''
     });
     // スクロール位置を元に戻す
-    $_global.scrollTop(currentScrollTop);
+    $window.scrollTop(currentScrollTop);
     // モーダルを隠す
-    var targetCloseModalId  = $(this).data('closemodal');
-    var $targetCloseOverlay = $('#' + targetCloseModalId);
-    var $targetCloseInner   = $('#' + targetCloseModalId + ' ' + modalInner);
+    var targetCloseModalName  = $(this).data('closemodal'),
+        $targetCloseOverlay = $('#' + targetCloseModalName),
+        $targetCloseInner   = $('#' + targetCloseModalName + ' ' + modalInner);
     $targetCloseOverlay.removeClass('active-modal');
     $targetCloseOverlay.fadeOut(FADEOUT_TIME);
     // モーダルウィンドウのスクロール位置をリセット
     $targetCloseInner.scrollTop(0);
   });
+
+  // arrowにカーソルが重なったら、arrowの色を変更。
+  // 後からappendした要素のhoverは、この描き方じゃないと動かない。
+  $(document).on('mouseenter','.modal-arrow',function() {
+    $(this).addClass('hover');
+  });
+  $(document).on('mouseleave','.modal-arrow',function() {
+    $(this).removeClass('hover');
+  });
+
 });
